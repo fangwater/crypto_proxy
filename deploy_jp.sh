133e528b7275ee8857e05d47bc3963ca0f6247db
fanghaizhou <fanghaizhou611@163.com>
2026-01-18 14:49:21 +0800

    fix

--- deploy_jp.sh
+++ deploy_jp.sh
@@ -33,12 +33,12 @@ DEPLOY_MODE=${DEPLOY_MODE:-full}
 SERVERS=(
     # "103.90.136.194:primary"
     # "103.90.136.195:secondary"
-    "156.231.137.90:primary"
-    "156.231.137.91:secondary"
+    # "156.231.137.90:primary"
+    # "156.231.137.91:secondary"
     # "103.90.136.196:primary"
     # "103.90.136.197:secondary"
-    #   "156.231.137.92:primary",
-    #   "156.231.137.188:secondary"
+    "156.231.137.92:primary",
+    "156.231.137.188:secondary"
 )
 
 user=el01
